fcd30654cf935c57502f699e30f444890332893c
Zaman Ali <138426327+zamanali423@users.noreply.github.com>
2026-09-16 15:36:59 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ const profile = {
   linkedin: "https://www.linkedin.com/in/zamanali27",
   resumeUrl:
     "https://drive.google.com/file/d/1EhV1YEzVYQhZo1WureoF7uOnzzK9EJso/view?usp=drive_link",
-  UpworkUrl: "zamanyaseen2024@gmail.com",
+  UpworkUrl: "https://www.upwork.com/freelancers/zamanali27",
   education: "Bachelors (Software Engineering)",
   achievements: [
     "Built Skilltrends AI Jobs & Interview Platform with 200+ registered users",
